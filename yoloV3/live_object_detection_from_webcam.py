d3e28501e65b9ebb75d5c78e1914806f2f8996be
mdurgasaianil <durgaanil7@gmail.com>
2022-01-19 00:49:26 +0530

    files upload

---
+++ yoloV3/live_object_detection_from_webcam.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 14 01:05:47 2021
+
+@author: Anil
+"""
+import cv2
+import numpy as np
+net = cv2.dnn.readNet('yolov3.weights','yolov3.cfg')
+classes = []
+with open('coco.names','r') as f:
+    classes = f.read().splitlines()
+
+#print(classes)
+# for to capturing the video
+cap = cv2.VideoCapture(0)
+# laoding the image
+#img = cv2.imread('image2.jpg')
+
+z = 0
+while cap.isOpened():
+    _, img = cap.read()
+    height, width, _ = img.shape
+    
+    # converting the image to give the input for yolo
+    blob = cv2.dnn.blobFromImage(img,1/255,(416,416),(0,0,0), swapRB=True,crop=False)
+    #for b in blob:
+    #    for n,img_blob in enumerate(b):
+    #        cv2.imshow(str(n), img_blob)
+    
+    net.setInput(blob)
+    output_layers_names = net.getUnconnectedOutLayersNames()
+    layerOutputs = net.forward(output_layers_names)
+    
+    boxes = []
+    confidences = []
+    class_ids = []
+    
+    for output in layerOutputs:
+        for detection in output:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.5:
+                center_x = int(detection[0]*width)
+                center_y = int(detection[1]*height)
+                w = int(detection[2]*width)
+                h = int(detection[3]*height)
+                
+                x = int(center_x - w/2)
+                y = int(center_y - h/2)
+                
+                boxes.append([x,y,w,h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+    #print(len(boxes)) 
+    
+    indexes = cv2.dnn.NMSBoxes(boxes,confidences,0.5,0.4)
+    #print(indexes.flatten())    
+    
+    font = cv2.FONT_HERSHEY_PLAIN  
+    colors = np.random.uniform(0,255,size=(len(boxes),3)) 
+    if len(indexes)>0:
+        for i in indexes.flatten():
+            x,y,w,h = boxes[i]
+            label = str(classes[class_ids[i]])
+            confidence = str(round(confidences[i],2))
+            color = colors[i]
+            cv2.rectangle(img,(x,y),(x+w,y+h),color,2)
+            cv2.putText(img, label + " " + confidence,(x,y+20),font,2,(255,255,255),2)
+    
+    cv2.imshow('Frame',img)
+    #cv2.imwrite('kang'+str(z)+'.jpg',img)
+    #z+=1
+   # key = cv2.waitKey(1)
+    if cv2.waitKey(10)==27:
+        cap.release()
+        cv2.destroyAllWindows()
+        break
